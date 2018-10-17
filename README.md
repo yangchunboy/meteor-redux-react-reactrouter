b1b95d48d0eb469175beae7b961e0ab0b3857f9e
chuck <121477964@qq.com>
2018-10-17 10:57:34 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # meteorApp
 meteor-redux-react-reactrouter a simple framework for meteor
+
+# 待完成
+1. meteor 1.8
+2. 支持react,redux,react-router
+3. 支持订阅和发布
+4. 使用meteor的账户系统
+5. 安装eslint
+6. 支持method获取数据
+7. 封装HTTP请求，支持restful api
+8. 支持less
+9. 支持es6语法
+10. 支持async/await
+11. 支持懒加载
---
+++ client/compatibility/plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @author chuck
+ * @date 2018/10/17
+ *
+ * his folder is for compatibility with JavaScript libraries that rely on variables
+ * declared with var at the top level being exported as globals.
+ * Files in this directory are executed without being wrapped in a new variable scope.
+ * These files are executed before other client-side JavaScript files
+ */
